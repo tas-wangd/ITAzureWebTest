becbc62612ace54c08e45316351a4099f903b036
Danny <wangd@tas.edu.tw>
2020-12-09 16:41:44 +0800

    no message

---
+++ 2020_winter_magazine_May/files/html/locales/config.js
@@ -0,0 +1,25 @@
+{
+  "locales": ["en", "ar", "de", "es", "fi", "fr", "he", "it", "ja", "nl", "no", "pl", "pt", "ru", "sv", "tr", "zh"],
+  "substitutes": {
+    "nb": "no",
+    "nn": "no",
+    "be": "ru"
+  },
+  "fonts": [{
+    "locales": ["de", "en", "es", "fi", "fr", "it", "nl", "no", "pl", "pt", "sv", "tr"],
+    "font-family": "Open Sans",
+    "subset": "latin-ext"
+  }, {
+    "locales": ["ar"],
+    "font-family": "Cairo",
+    "subset": "arabic"
+  }, {
+    "locales": ["he"],
+    "font-family": "Rubik",
+    "subset": "hebrew"
+  }, {
+    "locales": ["ru"],
+    "font-family": "Open Sans",
+    "subset": "cyrillic,cyrillic-ext"
+  }]
+}
